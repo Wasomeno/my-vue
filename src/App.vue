0d50054bdcfa183a3cf284eaea5cdcc01ddaecdc
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2020-02-04 17:04:25 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Practice1 />
+  </div>
+</template>
+
+<script>
+import Practice1 from './components/Practice1.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Practice1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
